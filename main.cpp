05e82eafd9df1d2d75e57303453c7daed7436393
Abhinav Verma <74522028+vermaabhinav363@users.noreply.github.com>
2023-02-20 19:03:14 +0530

    Test

---
+++ main.cpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+#define Mod(a,b) ((((a)%(b))+(b))%(b))
+#define ll long long
+#define vec vector<ll>
+#define pb push_back
+#define mk make_pair
+#define MAX2 9223372036854775806;
+const int MOD = 1000000007;
+const int MAX = 10000005;
+
+using namespace std;
+
+// Shortest Path 1
+
+
+int main()
+
+
+{
+    
+    //freopen("input.txt", "r", stdin);
+    //freopen("output.txt", "w", stdout);
+    ios_base::sync_with_stdio(false);
+    cin.tie(NULL);cout.tie(NULL);
+    ll t;
+    cin>>t;
+    while(t--){
+
+    }
+    
+}
+
